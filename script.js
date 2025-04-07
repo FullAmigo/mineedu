9abf7f6e450602e252f27d464c22ed1d7a4815ea
fullamigo5 <39329238+fullamigo5@users.noreply.github.com>
2025-04-07 10:57:20 +0900

    キャッシュ

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
     const searchButton = document.getElementById('search-button');
     const keywordInput = document.getElementById('keyword');
     const searchResultsContainer = document.getElementById('search-results');
-    const jsonFilePath = 'worlds.json?20250406';
+    const jsonFilePath = 'worlds.json?20250407';
     let jsonData = []; // JSONデータを格納する変数
 
     // JSONデータの読み込み
